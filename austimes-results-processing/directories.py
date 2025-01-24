c36d82045056b870b8aa909ff5ac481b90666f13
tim-graham-cwc <158250948+tim-graham-cwc@users.noreply.github.com>
2025-01-24 11:59:32 +1100

    Delete austimes-results-processing directory

--- austimes-results-processing/directories.py
+++
@@ -1,5 +0,0 @@
-class Directories:
-    def __init__(self):
-        self.INPUT_PATH = "inputs/" # set to whichever directory you want to drop input files into
-        self.OUTPUT_PATH = "outputs/" # set top whichever directory you want to drop output files to be exported to
-        self.MAPPING_PATH = "mapping/" # do not recommend changing
